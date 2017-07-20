e81c893016352b5cd7005599b2f5b6fdc357dfb3
Gray Pritchett <gray.pritchett@smsmt.com>
2017-07-20 11:29:47 +1000

    whitespace

--- nightowlsign.data/Models/Stores/StoreManager.cs
+++ nightowlsign.data/Models/Stores/StoreManager.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data.Entity;
 using System.Linq;
 using nightowlsign.data.Models.UpLoadLog;
-
 
 namespace nightowlsign.data.Models.Stores
 {
